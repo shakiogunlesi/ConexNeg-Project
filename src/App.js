fcf28633987d236bb3d8f3573e81c99a0dfcd6cc
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-04-18 12:04:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/ConexNeg-Project" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/landingPage" element={<LandingPage />} />
       </Routes>
     </>
